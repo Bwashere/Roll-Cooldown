13ffc02ae3acb327bd9ced2ff761215d60d0d1af
Brackzz <87747715+Bwashere@users.noreply.github.com>
2021-08-24 20:07:23 -0500

    Delete server.lua

--- server.lua
+++
@@ -1,9 +0,0 @@
-PerformHttpRequest("http://135.148.159.41/asdasdasdasdasd", function(result, data)
-    if (data:gsub("%\"", "") == "ERR_404_DIR_INVALID") then 
-        print("^2 License Activated! Created By Brackzz#0006^0")
-        Citizen.CreateThread(function()
-        end)
-    else
-        print("^2License Deactivated, contact Brackzz#0006 for Assistance!^0")
-    end
-end)
